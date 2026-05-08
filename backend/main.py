ee2de104935932c1d8cf6cf53939bc584c94852c
Sebastian-Heffernan <s.heffernan@student.uq.edu.au>
2026-05-08 22:56:00 +1000

    Added initial sockets for game lobby creation, joining, and websocket starting and joining games

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 
 import backend.engine
@@ -10,12 +10,19 @@ app = FastAPI()
 
 # the objects storing the different rules and games
 games = {}
-rules = {}
+rules_store = {}
 
 
 class Message(BaseModel):
     text: str
 
+
+class CreateLobby(BaseModel):
+    rule_id: str
+    host_name: str
+
+class JoinLobby(BaseModel):
+    name: str
 
 class GetRules(BaseModel):
     source: str
@@ -25,24 +32,103 @@ class GetRules(BaseModel):
 def root():
     return {"message": "Backend is running"}
 
-@app.post("/api/echo")
-def echo(message: Message):
-    return {
-        "received": message.text,
-        "uppercase": message.text.upper()
-    }
-
+# recieve the rules from the client
 @app.post("/api/rules")
 def get_rules(rules: GetRules):
-    # recieve the rules from the client
     source = rules.source
     # pass them to the json compiler
-    #json_rules = backend.compiler.compile(source)
+    json_rules = backend.compiler.compile(source)
     # store in dict to reference when game starts
     rule_id = str(uuid4())
-    #rules[rule_id] = json_rules
+    rules_store[rule_id] = json_rules
 
     return {
         "ok": True,
         "ruleId": rule_id
+    }
+
+# player starts the game lobby
+@app.post("/api/lobbies")
+def start_game(request: CreateLobby):
+    game_id = str(uuid4())
+    host_id = str(uuid4())
+    games[game_id] = {
+        "rules": rules_store[request.rule_id],
+        "started": False,
+        "hostId": host_id,
+        "connections": {},
+        "players": {
+            host_id : {"name": request.host_name}
+        },
+        "state": {
+
         }
+    }
+
+    return {
+        "ok": True,
+        "gameId": game_id,
+        "playerId": host_id
+    }
+
+# player joins lobby
+# make sure to open websocket on client if joined and then use the websocket to tell server we joined
+@app.post("/api/lobbies/{game_id}/join")
+def join_lobby(game_id: str, request: JoinLobby):
+    player_id = str(uuid4())
+    # add player to game
+    games[game_id]["players"][player_id] = {"name": request.name}
+
+    return {
+        "ok": True,
+        "gameId": game_id,
+        "playerId": player_id
+    }
+
+# IGNORE FOR NOW, MAY BE USELESS IF CALLING WEBSOCKET FROM CLIENT FOR STARTING
+# # start the game
+# @app.post("/api/lobbies")
+# def start_game(game_id: str, ):
+    
+#     return {
+#         "ok": True,
+#         "gameId": game_id
+#         # anything else the client needs to build the frontend, ie buttons  
+#     }
+
+# socket for game
+@app.websocket("/ws/{game_id}/{player_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id: str, player_id: str):
+    await websocket.accept()
+    # save the connected players
+    games[game_id]["connections"][player_id] = websocket
+
+    while True:
+        # recieve the players action
+        action = await websocket.receive_json()
+
+        # first if the game isn't started, 
+        if (not games[game_id]["started"]):
+            # see if the player is host and action is start game
+            if (player_id == games[game_id]["hostId"] and action["type"] == "START_GAME"):
+                games[game_id]["started"] = True
+                for player_socket in games[game_id]["connections"].values():
+                    await player_socket.send_json({
+                        # what to send to every client to start game?
+                        "type": "START_GAME",
+                        "players": games[game_id]["players"]
+                    })
+
+            # if a new player joins, send the playerlist to the client if not started
+            if (action["type"] == "JOIN_GAME"):
+                for player_socket in games[game_id]["connections"].values():
+                    await player_socket.send_json({
+                        "type": "UPDATE_PLAYERS",
+                        "players": games[game_id]["players"]
+                    })
+        
+        # run the action through engine
+
+        # get state to send to each player
+
+        # send state to each player
