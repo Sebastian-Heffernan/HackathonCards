237ea62ee2599151adea63739b05da269d703d3b
Sebastian-Heffernan <s.heffernan@student.uq.edu.au>
2026-05-08 20:17:42 +1000

    Added inital structure for managing games and rules, added post request for recieving the rules.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
+from uuid import uuid4
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import backend.engine
+import backend.compiler
+
 app = FastAPI()
+
+# the objects storing the different rules and games
+games = {}
+rules = {}
 
 
 class Message(BaseModel):
     text: str
+
+
+class GetRules(BaseModel):
+    source: str
 
 
 @app.get("/")
@@ -18,3 +31,18 @@ def echo(message: Message):
         "received": message.text,
         "uppercase": message.text.upper()
     }
+
+@app.post("/api/rules")
+def get_rules(rules: GetRules):
+    # recieve the rules from the client
+    source = rules.source
+    # pass them to the json compiler
+    #json_rules = backend.compiler.compile(source)
+    # store in dict to reference when game starts
+    rule_id = str(uuid4())
+    #rules[rule_id] = json_rules
+
+    return {
+        "ok": True,
+        "ruleId": rule_id
+        }
