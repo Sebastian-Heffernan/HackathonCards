604680d3efb9114b012e8e5777b17077cdd90109
Sebastian-Heffernan <s.heffernan@student.uq.edu.au>
2026-05-09 20:04:53 +1000

    Added working button actions (finally).

--- frontend/src/lib/components/Game.svelte
+++ frontend/src/lib/components/Game.svelte
@@ -31,7 +31,7 @@
   });
 
   const toggleCard = (i: number) => {
-    selected[i] = !selected[i];
+    selected = selected.map((_, idx) => idx === i ? !selected[i] : false);
   };
 
   const playAction = (actionIdx: number) => {
@@ -41,20 +41,34 @@
     sendAction(action, chosenCards);
   };
 </script>
-
+<div class="absolute top-4 right-4 bg-white text-black p-4 rounded shadow max-w-xl max-h-96 overflow-auto z-50">
+  <p class="font-bold mb-2">Debug playerState</p>
+  <pre>{JSON.stringify(playerState, null, 2)}</pre>
+</div>
 <div class="relative w-screen h-screen p-4 bg-green-500">
   <div class="nes-container with-title absolute t-0 l-0 w-40 h-full is-dark">
     <p class="title">Decks</p>
     <Deck />
   </div>
 
-  <div class="absolute bottom-4 left-1/2 -translate-x-1/2">
+  <div class="absolute bottom-4 left-1/2 -translate-x-1/2 flex flex-col items-center gap-4">
     <Hand
-      actions={playerState.actions}
+      actions={[]}
       cards={playerState.hand}
       onCardClick={toggleCard}
       {selected}
-      {playAction}
     />
+
+    <div class="flex gap-4">
+      {#each playerState.actions as action, actionIdx}
+        <button
+          type="button"
+          class="nes-btn is-primary"
+          onclick={() => playAction(actionIdx)}
+        >
+          {action}
+        </button>
+      {/each}
+    </div>
   </div>
 </div>
