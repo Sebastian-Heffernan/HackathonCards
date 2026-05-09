10821de6f6c262c9582861169fedc1b7f64f1b8e
Coup3z-pixel <coup3z@coup3z.>
2026-05-09 16:01:57 +1000

    finished basic simple game renderer with actions and card selection

--- frontend/src/lib/components/Game.svelte
+++ frontend/src/lib/components/Game.svelte
@@ -3,68 +3,90 @@
 This is a demo of blackjack
 
 -->
-<script>
+<script lang="ts">
 import Hand from "./Hand.svelte";
 import Deck from "./Deck.svelte";
 import { Suits } from "$lib/suit.ts";
 
-const gameState = {
+import { writable } from 'svelte/store';
 
+const gameState = {
+  actions: ["Hit", "Stand"],
+  masked_cards: [[{
+    "suit": Suits.SPADES, 
+    "value": "UNKNOWN"
+  }, {
+      "suit": Suits.SPADES, 
+      "value": "UNKNOWN"
+    }, {
+      "suit": Suits.SPADES, 
+      "value": "UNKNOWN"
+    }, {
+      "suit": Suits.SPADES, 
+      "value": "UNKNOWN"
+    }, {
+      "suit": Suits.SPADES, 
+      "value": "UNKNOWN"
+    }]],
+  cards: [{
+    "suit": Suits.SPADES, 
+    "value": "A"
+  }, {
+      "suit": Suits.SPADES, 
+      "value": "02"
+    }, {
+      "suit": Suits.SPADES, 
+      "value": "02"
+    }, {
+      "suit": Suits.SPADES, 
+      "value": "02"
+    }, {
+      "suit": Suits.SPADES, 
+      "value": "02"
+    }]
 };
 
-const actions = ["Hit", "Stand"];
-const opponent_cards = [{
-  "suit": Suits.SPADES, 
-  "value": "UNKNOWN"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "UNKNOWN"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "UNKNOWN"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "UNKNOWN"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "UNKNOWN"
-}];
-const cards = [{
-  "suit": Suits.SPADES, 
-  "value": "A"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "02"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "02"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "02"
-}, {
-  "suit": Suits.SPADES, 
-  "value": "02"
-}]
+// Use a reactive state for the selection array
+let selected = $state(
+  gameState?.cards?.length 
+    ? new Array(gameState.cards.length).fill(false) 
+    : []
+);
+const toggleCard = (i: number) => {
+  console.log("HIHIHI");
+  selected[i] = !selected[i]; // This will now trigger UI updates
+};
+
+const isCardSelected = (i: number) => {
+  return selected[i];
+};
+
+const playAction = (actionIdx: number) => {
+  // Filter cards to find which ones are currently true
+  const chosenCards = gameState.cards.filter((_, i) => selected[i]);
+  console.log("Playing action with:", chosenCards);
+};
+
 </script>
 
 <div class="relative w-screen h-screen p-4 bg-green-500">
   <!--  -->
-  <div class="nes-container with-title absolute t-0 l-0 w-40">
+  <div class="nes-container with-title absolute t-0 l-0 w-40 h-full is-dark">
     <p class="title">Decks</p>
     <Deck />
-  </div>
-  <!--  -->
-  <div class="absolute t-0 r-0">
-
   </div>
 
   <!-- Your Hand -->
   <div class="absolute bottom-4 left-1/2 -translate-x-1/2">
-    <Hand actions={actions} cards={cards} />
+    <Hand actions={gameState.actions} cards={gameState.cards} onCardClick={toggleCard} {selected} {playAction} />
   </div>
 
   <!-- Opponent Hand -->
-  <div class="absolute top-4 left-1/2 -translate-x-1/2">
-    <Hand actions={actions} cards={opponent_cards} isOpponent={true} />
-  </div>
+  {#each gameState.masked_cards as opponent_cards}
+     <!-- content here -->
+    <div class="absolute top-4 left-1/2 -translate-x-1/2">
+      <Hand actions={[]} cards={opponent_cards} isOpponent={true} selected={[]} />
+    </div>
+  {/each}
+  
 </div>
