0e9f51aeeed19652e712d71bec0aba6210295089
BlueOrcaz <bondoldman@gmail.com>
2026-05-10 02:55:31 +1000

    Updated Documentation to reflect new functionality.

--- frontend/src/lib/docsData.ts
+++ frontend/src/lib/docsData.ts
@@ -11,31 +11,31 @@ export const docsData = {
     items: {
       "ASSERT": {
         name: "ASSERT",
-        description: "Makes a label a player's action",
-        usage: "ASSERT [label: str] [player: int]",
+        description: "Makes a label into the player's action.\n Additional arguments can be left empty in order to call assert for all players.",
+        usage: "ASSERT[label: str][[OPTIONAL] player: int]",
         example: "ASSERT label1 0",
       },
       "CALL": {
         name: "CALL",
-        description: "Starts processing label",
+        description: "Jumps to a specific label and process, while retaining where it left off.\n Requires calling RETURN in order to return back to where it left off.\n",
         usage: "CALL [label: str]",
         example: "CALL label2",
       }, 
       "COMPARE": {
         name: "COMPARE",
-        description: "Sets x = x + y for COMPARE x + y.\nOn true executes n+1. On false, n+2",
-        usage: "COMPARE [x: int][operator: Enum][y: int]",
+        description: "Sets x = x + y for COMPARE x + y.\nOn true executes n+1. On false, n+2.\n Operators Include '==', '!=', '>', '<', '>=', '<='.",
+        usage: "COMPARE [x: int][operator: '==' | '!=' | '>' | '<' | '>=' | '<=' |][y: int]",
         example: "COMPARE x + y"
       },
       "DECK": {
         name: "DECK",
-        description: "Controls a specified deck by name through an action specified",
-        usage: "DECK [(MAKE | SHUFFLE): str][name: str]",
+        description: "Controls a specified deck by name through an action specified.",
+        usage: "DECK [('MAKE' | 'SHUFFLE' | 'CLEAR' | 'RESET'): str][name: str]",
         example: "DECK MAKE deck1"
       }, 
       "DRAW": {
         name: "DRAW",
-        description: "Takes a card from deck name to a players hand by number of times",
+        description: "Takes a card from deck name to a players hand by a given number of times.",
         usage: "DRAW [deck_name: str][player_id: int][times: int]",
         example: "DRAW deck1 0 1"
       },
@@ -47,68 +47,68 @@ export const docsData = {
       },
       "GOTO": {
         name: "GOTO",
-        description: "Jumps process to a given label.",
+        description: "Jumps process to a given label, and begins processing its data.\n Does not retain its previous location during execution.\nSee CALL for more deatils.",
         usage: "GOTO [LABEL]",
         example: "GOTO LABEL"
       },
       "HANDLEN": {
         name: "HANDLEN",
-        description: "Stores hand length of player, 'p' in VAR",
+        description: "Stores the hand length of player, 'p' in VAR",
         usage: "VALUE [VAR][p: int][c: int]",
         example: "VALUE CARDPLACE 0 1",
       },
       "MATH": {
         name: "MATH",
         description: "Performs mathematical operations between two labels, such as \n addition, subtraction, multiplication, division and modulus",
-        usage: "MATH [LABEL1][ + | - | * | / | % ][LABEL2]",
+        usage: "MATH [x: str][enum: '+' | '-' | '*' | '/' | '%' ][y: str]",
         example: "MATH X + Y # Equivalent would be X = X + Y"
       },
       "MOVE": {
         name: "MOVE",
-        description: "Moves a card from a player's hand and then move it to a specified deck",
-        usage: "MOVE [Deck][PlayerID][CardID]",
+        description: "Moves a card from a player's hand and then move it to a specified deck.\n Also resets its global visibility flag.",
+        usage: "MOVE [Deck: str][PlayerID: int][CardID: int]",
         example: "MOVE DISCARD 1 2 # Move a card to the discard pile from player 1, card 2"
       },
       "REVEAL": {
         name: "REVEAL",
-        description: "Specify a player's most recent drawn card (-1)",
-        usage: "REVEAL [PlayerID]",
+        description: "Specify a player's most recently drawn card (Lastmost Index).\n Sets its global visibility flag to TRUE for everybody.",
+        usage: "REVEAL [PlayerID: int]",
         example: "REVEAL 2 # Show Player 2's Last Card"
       },
       "SUIT": {
         name: "SUIT",
-        description: "Stores the suit of the card of player, p, in their hand at c into VAR",
-        usage: "SUIT [VAR][p: int][c: int]",
+        description: "Stores the suit of the card of player [p], in their hand at [c] into [VAR]",
+        usage: "SUIT [VAR: str][p: int][c: int]",
         examples: "SUIT VARSUIT 0 1"
       },
       "VALUE": {
         name: "VALUE",
         description: "Stores the value of the card at VAR",
-        usage: "VALUE [VAR][p: int][c: int]",
+        usage: "VALUE [VAR: str][p: int][c: int]",
         examples: "VALUE VARVALUE 0 1"
       },
       "VARG": {
         name: "VARG",
         description: "Declares a 'Game State' Variable. Server-Sided/Global Variable",
-        usage: "VARG [SET][NAME][VALUE]",
+        usage: "VARG [SET][name: str][value: int/str]",
         example: "VARG TOTAL 5 # Everyone can access this value"
       },
       "VARP": {
         name: "VARP",
         description: "Declares a Variable related to a given player.\n Client-Sided/Local Variable",
-        usage: "VARP [SET][playerID][name][value]",
+        usage: "VARP [SET][playerID: int][name: str][value: str]",
         example: "VARP 2 TOTAL 5 # Player 2 has a TOTAL of 5"
       },
       "SHOWVAR": {
         name: "SHOWVAR",
-        description: "Adds a variable to showVars array, to then be shown to every client",
-        usage: "SHOWVAR [NAME]",
+        description: "Adds a variable to showVars array, a global visibility array, to then be shown to every client",
+        usage: "SHOWVAR [name: str]",
         example: "VARP 2 TOTAL 5 # Player 2 has a TOTAL of 5"
       },
       "RETURN": {
         name: "RETURN",
-        description: "Returns back to the last CALL on the stack trace",
-        usage: "VARP [SET][playerID][name][value]",
+        description: "Returns back to the last CALL on the stack trace.\n Works in Unison with CALL command.",
+        usage: "VARP [SET][playerID: int][name: str][value: str/int]",
         example: "VARP 2 TOTAL 5 # Player 2 has a TOTAL of 5"
       },
     }
