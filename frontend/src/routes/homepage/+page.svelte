0d171e4b28ca6a650f935ecafeb4f3fbeaebf01e
aarsh <aarsh2007@gmail.com>
2026-05-09 14:15:44 +1000

    Added base homepage with title, button and header

---
+++ frontend/src/routes/homepage/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<!-- Page Wrapped -->
+<div class = "min-h-screen bg-gray-100 flex flex-col">
+
+
+<!-- Header -->
+ <header class="flex items-center justify-between p-4 bg-white shadow text-center">
+
+    <!-- Title - Cardssembly -->
+    <h1 class="text-2xl font-bold text-center"
+    >Cardssembly</h1>
+
+     <!-- Create Lobby Button -->
+      <button class="text-3xl font-bold px-3 py-1 bg-blue-500 text-white rounded text-center">
+      +
+      </button>
+    
+ </header>
+
+ <main class="flex-1 flex flex-col items-center p-6">
+    <!-- Section title -->
+    <h2 class ="text-xl font-semibold mb-4">
+        Available Lobbies
+    </h2>
+ </main>
+ </div>
