e453cb3574c0a041ae5077339e02fd87934c0f1e
Coup3z-pixel <coup3z@coup3z.>
2026-05-09 12:50:28 +1000

    refactor code to multi file caching"

--- backend/routers/lobby.py
+++ backend/routers/lobby.py
@@ -12,6 +12,7 @@ async def get_lobbies():
     for lobby_id, game_id in lobby_codes.items():
         # Get the game object using the game_id
         game_data = games.get(game_id)
+        print(lobby_id)
 
         if game_data:
             # Count the players in that specific game
@@ -20,6 +21,7 @@ async def get_lobbies():
             # Create the formatted string
             summary.append(
                 {
+                    "lobby_id": lobby_id,
                     "lobby_info": f"Lobby {lobby_id} (Game: {game_id})",
                     "player_count": player_count,
                 }
